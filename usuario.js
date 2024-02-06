00ca9b8898c43cf21d263b1408e0a3ea896e8bb9
NUÑEZ NOVOA DEIMER ANDRES <deandre.nn@gmail.com>
2024-02-05 20:07:29 -0500

    Formaulario para agregar usuario a localstorage

--- usuario.js
+++ usuario.js
@@ -1,15 +1,59 @@
-let url = 'https://jsonplaceholder.typicode.com/users/';
-fetch(url)
-    .then( response => response.json() )
-    .then( data => mostrarData(data) )
-    .catch( error => console.log(error) )
+let url = "https://jsonplaceholder.typicode.com/users/";
+
+const getUsuarios = () => {
+    return JSON.parse(localStorage.getItem("usuarios"));
+}
+
+const agregarUsuario = (e) => {
+    e.preventDefault();
+    const data = new FormData(e.target);
+    const dataObject = Object.fromEntries(data.entries());
+    const users = getUsuarios();
+
+   const newUser = {
+        "id": 1,
+        "name": dataObject.name,
+        "username": "Bret",
+        "email": dataObject.email,
+        "address": {
+            "street": "Kulas Light",
+            "suite": "Apt. 556",
+            "city": "Gwenborough",
+            "zipcode": "92998-3874",
+            "geo": {
+                "lat": "-37.3159",
+                "lng": "81.1496"
+            }
+        },
+        "phone": dataObject.phone,
+        "website": dataObject.website,
+        "company": {
+            "name": "Romaguera-Crona",
+            "catchPhrase": "Multi-layered client-server neural-net",
+            "bs": "harness real-time e-markets"
+        }
+    }
+
+    users.push(newUser);
+    console.log("e", dataObject, users);
+    localStorage.setItem("usuarios", JSON.stringify(users));
+    mostrarData(users);
+    e.target.reset();
+}
+
+var agregarForm = document.getElementById("agregarForm");
+
+agregarForm.addEventListener("submit",(e) =>  agregarUsuario(e));
+
+console.log(agregarForm);
 
 const mostrarData = (data) => {
-    console.log(data)
-    let body = ''; // Inicializar el contenido HTML
+    localStorage.setItem("usuarios", JSON.stringify(data));
+	console.log(data);
+	let body = ""; // Inicializar el contenido HTML
 
-    for (let i = 0; i < data.length; i++) {
-        body += `<div class="card-user">
+	for (let i = 0; i < data.length; i++) {
+		body += `<div class="card-user">
         <a id="eliminarBtn" class="card-user__button" href="#">x</a>
                     <div class="fila">
                         <div class="usuario">usuario ${data[i].id}</div>
@@ -23,23 +67,33 @@ const mostrarData = (data) => {
 
                     </div>
                  </div>`;
+	}
+
+	// 'body' ahora contiene el HTML de las cajas generadas dinámicamente
+	document.getElementById("data").innerHTML = body;
+	//console.log(body)
+};
 
 
-              
-                }
-            
-                // 'body' ahora contiene el HTML de las cajas generadas dinámicamente
-                console.log(body);
-                document.getElementById('data').innerHTML = body
-                //console.log(body)
-            }
-                             
+let dataUsuarios = getUsuarios();
+
+
+if (!dataUsuarios){
+    fetch(url)
+	.then((response) => response.json())
+	.then((data) => mostrarData(data))
+	.catch((error) => console.log(error));
+
+} else {
+    console.log("entro en el else")
+    mostrarData(dataUsuarios);
+}
 
 
 
-   // 'body' ahora contiene el HTML de las cajas generadas dinámicamente
-   document.getElementById('data').innerHTML = body
-   //console.log(body)
+// 'body' ahora contiene el HTML de las cajas generadas dinámicamente
+
+//console.log(body)
 
 // Botón que abre el modal
 var span = document.getElementById("actulizar");
@@ -53,15 +107,7 @@ var card = document.getElementById("card");
 // card.addEventListener("click",function() {
 // console.log("entro");
 // card.style.display = "none";
- 
+
 // });
 
 var a = document.getElementById("eliminarBtn");
-
-
-
-
-    }
-    
-
-}
