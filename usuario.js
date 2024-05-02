49a8b0fa5282c7622f566d3e2a54b7a87c2a3169
NUÑEZ NOVOA DEIMER ANDRES <deandre.nn@gmail.com>
2024-05-02 12:50:18 -0500

    Mejoras en el código

--- usuario.js
+++ usuario.js
@@ -1,82 +1,74 @@
 let url = "https://jsonplaceholder.typicode.com/users/";
 
 const getUsuarios = () => {
-    return JSON.parse(localStorage.getItem("usuarios"));
-}
+	return JSON.parse(localStorage.getItem("usuarios"));
+};
 
 const agregarUsuario = (e) => {
-    e.preventDefault();
-    const data = new FormData(e.target);
-    const dataObject = Object.fromEntries(data.entries());
-    const users = getUsuarios();
+	e.preventDefault();
+	const data = new FormData(e.target);
+	const dataObject = Object.fromEntries(data.entries());
+	const users = getUsuarios();
 
-   const newUser = {
-        "id": 1,
-        "name": dataObject.name,
-        "username": "Bret",
-        "email": dataObject.email,
-        "address": {
-            "street": "Kulas Light",
-            "suite": "Apt. 556",
-            "city": "Gwenborough",
-            "zipcode": "92998-3874",
-            "geo": {
-                "lat": "-37.3159",
-                "lng": "81.1496"
-            }
-        },
-        "phone": dataObject.phone,
-        "website": dataObject.website,
-        "company": {
-            "name": "Romaguera-Crona",
-            "catchPhrase": "Multi-layered client-server neural-net",
-            "bs": "harness real-time e-markets"
-        }
-    }
+	const newUser = {
+		id: 1,
+		name: dataObject.name,
+		username: "Bret",
+		email: dataObject.email,
+		address: {
+			street: "Kulas Light",
+			suite: "Apt. 556",
+			city: "Gwenborough",
+			zipcode: "92998-3874",
+			geo: {
+				lat: "-37.3159",
+				lng: "81.1496",
+			},
+		},
+		phone: dataObject.phone,
+		website: dataObject.website,
+		company: {
+			name: "Romaguera-Crona",
+			catchPhrase: "Multi-layered client-server neural-net",
+			bs: "harness real-time e-markets",
+		},
+	};
 
-    users.push(newUser);
-    console.log("e", dataObject, users);
-    localStorage.setItem("usuarios", JSON.stringify(users));
-    mostrarData(users);
-    e.target.reset();
-}
+	users.push(newUser);
+	localStorage.setItem("usuarios", JSON.stringify(users));
+	mostrarData(users);
+	e.target.reset();
+};
 
 var agregarForm = document.getElementById("agregarForm");
 
-agregarForm.addEventListener("submit",(e) =>  agregarUsuario(e));
+agregarForm.addEventListener("submit", (e) => agregarUsuario(e));
 
 window.addEventListener("load", () => {
-    asignarEventoEliminar();
-})
+	asignarEventoEliminar();
+});
 
 const asignarEventoEliminar = () => {
-    const btnsDelete = document.querySelectorAll(".card-user__button");
+	const btnsDelete = document.querySelectorAll(".card-user__button");
 
-    
-    btnsDelete.forEach(x => {
-        x.addEventListener("click", (e) => { 
-            console.log(e);
-            var id = e.target.id;
-            var idUsuario = id.split("_")[1]; 
-            var users = getUsuarios();
+	btnsDelete.forEach((x) => {
+		x.addEventListener("click", (e) => {
+			var id = e.target.id;
+			var idUsuario = id.split("_")[1];
+			var users = getUsuarios();
 
-            users = users.filter(x => x.id != idUsuario);
-            localStorage.setItem("usuarios", JSON.stringify(users));
-            mostrarData(users);
-        });
-    })
-}
-
-
-
+			users = users.filter((x) => x.id != idUsuario);
+			localStorage.setItem("usuarios", JSON.stringify(users));
+			mostrarData(users);
+		});
+	});
+};
 
 const mostrarData = (data) => {
-    localStorage.setItem("usuarios", JSON.stringify(data));
-	console.log(data);
-	let body = ""; // Inicializar el contenido HTML
-
-    for (let i = 0; i < data.length; i++) {
-        body += `<div class="card-user">
+	localStorage.setItem("usuarios", JSON.stringify(data));
+    let body = "";
+	for (let i = 0; i < data.length; i++) {
+		body += `<div class="card-user">
         <a id="eliminarBtn_${data[i].id}" class="card-user__button" href="#">X</a>
                     <div class="fila">
                         <div class="usuario">usuario ${data[i].id}</div>
@@ -89,28 +81,21 @@ const mostrarData = (data) => {
                         <span>
                     </div>
                  </div>`;
-    }
+	}
 
 	// 'body' ahora contiene el HTML de las cajas generadas dinámicamente
 	document.getElementById("data").innerHTML = body;
-    asignarEventoEliminar();
+	asignarEventoEliminar();
 	//console.log(body)
 };
 
-
 let dataUsuarios = getUsuarios();
 
-
-if (!dataUsuarios){
-    fetch(url)
-	.then((response) => response.json())
-	.then((data) => mostrarData(data))
-	.catch((error) => console.log(error));
-
+if (!dataUsuarios) {
+	fetch(url)
+		.then((response) => response.json())
+		.then((data) => mostrarData(data))
+		.catch((error) => console.log(error));
 } else {
-    console.log("entro en el else")
-    mostrarData(dataUsuarios);
+	mostrarData(dataUsuarios);
 }
-
-
-
